fa4669a87a366dfb1fa7092336df2b146d89ac40
kennedy <x.johnkennedy.x@gmail.com>
2024-01-25 14:11:18 +0300

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { createServer } from 'http';
+
+dotenv.config({ path: './config.env' });
+
+const PORT = process.env.PORT;
+const DB = process.env.DATABASE;
+
+const app = express();
+const httpServer = createServer(app);
+
+httpServer.listen(PORT, () => {
+   console.log(`Server listening on port ${PORT}`);
+});
+
+mongoose
+   .connect(DB as string)
+   .then(() => {
+      console.log('DB connected succesfully');
+   })
+   .catch((error) => {
+      console.log(error);
+   });
